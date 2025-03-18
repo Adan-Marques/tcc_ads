28a57049dc4865c41c3f474662d1ed388485f1a3
Adan-Marques <adan__marx@hotmail.com>
2025-03-17 22:37:47 -0300

    feat register

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.views import logout_then_login
-#from django.contrib.auth.tokens.PasswordResetTokenGenerator import default_token_generator
+from users.models import User
 
 def home(request):
     return render(request, 'home.html')
@@ -10,4 +10,16 @@ def logout_view(request):
     return logout_then_login(request)
 
 def cadastro(request):
+    user = User()
+    
+    if request.method == 'POST':
+        user.cpf = request.POST.get('cpf')
+        user.first_name = request.POST.get('nome')
+        user.last_name = request.POST.get('sobrenome')
+        user.telefone = request.POST.get('telefone')
+        user.endereco = request.POST.get('endereco')
+        user.password = request.POST.get('senha')
+        user.set_password(user.password)
+        user.save()
+
     return render(request, 'cadastro.html')
