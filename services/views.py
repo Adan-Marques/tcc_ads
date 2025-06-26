713e40eee3fc3e93a4888ea37506b14d16e59d22
Adan-Marques <adan__marx@hotmail.com>
2025-06-26 18:05:20 -0300

    Fix

--- services/views.py
+++ services/views.py
@@ -182,7 +182,7 @@ def minhas_avaliacoes(request):
     return render(request, 'cliente/minhasAvaliacoes.html', )
 
 def gerenciarTicketPrestador(request): 
-    service_user = Service.objects.filter(status='E', prestador=request.user)
+    service_user = Service.objects.filter(status__in=['E','P'], prestador=request.user)
     historico = Service.objects.filter(status='F', prestador=request.user)
     context = {
         'service_user': service_user,
